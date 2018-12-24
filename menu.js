4354a8b5ab6725510639d9c42bee4a0a276f9f79
Jonas Johansson <jns.johansson@gmail.com>
2018-12-24 11:44:56 +0100

    Added Download button

--- menu.js
+++ menu.js
@@ -23,6 +23,20 @@ for (let btn of btns) {
 			case 'align-right':
 				cover.style.textAlign = 'right';
 				break;
+			case 'download':
+				let dataUrl = document.querySelector('canvas').toDataURL('image/png');
+				domtoimage
+					.toPng(cover)
+					.then(function(dataUrl) {
+						var link = document.createElement('a');
+						link.download = `test.png`;
+						link.href = dataUrl;
+						link.click();
+					})
+					.catch(function(error) {
+						console.error(error);
+					});
+				break;
 		}
 	});
 }
